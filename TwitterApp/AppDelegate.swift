3be2de391314bfa755458306932f90249841b392
Chhaya Tiwari <apple@Chhaya-Tiwari.local>
2018-10-12 00:15:21 +0530

    App Delegate class and info.plist file major update

--- TwitterApp/AppDelegate.swift
+++ TwitterApp/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import TwitterKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,10 +16,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // (API key)
+        // (API secret key)
         // Override point for customization after application launch.
+        TWTRTwitter.sharedInstance().start(withConsumerKey: "8p3hJCcfpNdh7tsbxpExEDJmQ", consumerSecret: "7CBexXecfvww0P6A6hvgsbG2joqAEAdk1ReIDtE8WlA9y51AEL")
         return true
     }
 
+     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return TWTRTwitter.sharedInstance().application(app, open: url, options: options)
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
