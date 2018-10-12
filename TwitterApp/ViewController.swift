89a21eabd8ab7f863d4f38755ed62ffda208019a
Chhaya Tiwari <apple@Chhaya-Tiwari.local>
2018-10-12 13:33:08 +0530

    extract userinfo such as name and profile picture

--- TwitterApp/ViewController.swift
+++ TwitterApp/ViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import TwitterKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var profilePicture: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var screenName: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +23,22 @@ class ViewController: UIViewController {
     }
 
     @IBAction func loginTwitter(_ sender: Any) {
+        TWTRTwitter.sharedInstance().logIn { (session, error) in
+            if session != nil {
+                if let userId = session?.userID {
+                    let client = TWTRAPIClient(userID: userId)
+                    client.loadUser(withID: userId, completion: { (user, error) in
+                        if error == nil {
+                            self.userName.text = user?.name
+                            self.screenName.text = user?.screenName
+                            print(user?.name, user?.screenName, user?.profileImageLargeURL)
+                            let image = 
+                        }
+                    })
+                    
+                }
+            }
+        }
     }
     
 }
